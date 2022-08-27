f4d336a8a19b78d9b688f1fa946474c195b3ddb7
lkhue65 <95577495+lkhue65@users.noreply.github.com>
2022-08-27 20:37:41 +0700

    Add files via upload

---
+++ ChuyenBay.java
@@ -0,0 +1,51 @@
+package maybay;
+import java.util.Scanner;
+public class ChuyenBay extends TuyenBay {
+	protected  String machuyenBay;
+	protected String noiDi;
+	protected String noiDen;
+	protected String ngaykhoiHanh;
+	protected  String cong;
+	public ChuyenBay() {
+		
+	}
+	public ChuyenBay(String machuyenBay, String noiDi, String noiDen, String ngaykhoiHanh, String cong) {
+		this.machuyenBay = machuyenBay;
+		this.noiDi = noiDi;
+		this.noiDen = noiDen;
+		this.ngaykhoiHanh = ngaykhoiHanh;
+		this.cong = cong;
+	}
+	public String getMachuyenBay() {
+		return machuyenBay;
+	}
+	public void setMachuyenBay(String machuyenBay) {
+		this.machuyenBay = machuyenBay;
+	}
+	public String getNoiDi() {
+		return noiDi;
+	}
+	public void setNoiDi(String noiDi) {
+		this.noiDi = noiDi;
+	}
+	public String getNoiDen() {
+		return noiDen;
+	}
+	public void setNoiDen(String noiDen) {
+		this.noiDen = noiDen;
+	}
+	public String getNgaykhoiHanh() {
+		return ngaykhoiHanh;
+	}
+	public void setNgaykhoiHanh(String ngaykhoiHanh) {
+		this.ngaykhoiHanh = ngaykhoiHanh;
+	}
+	public String getCong() {
+		return cong;
+	}
+	public void setCong(String cong) {
+		this.cong = cong;
+	}
+public void nhap()
+{
+
